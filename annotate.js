bd914b01eda30cb7186ba5d3ddd16edd26eaf7fc
Tobias Kolditz <tbs.kldtz@gmail.com>
2018-07-06 22:14:58 +0200

    Display information on page
    Sending info to content script and modifying DOM (inserting simple DIV
    element as first child of body).

--- annotate.js
+++ annotate.js
@@ -1,1 +1,39 @@
+chrome.runtime.onMessage.addListener(function (message, sender) {
+    clearInfoDiv();
+    var data = message.data;
+    addTitle(data.title);
+    if (data.definitions) {
+        addDefinitions(data.definitions);
+    }
+});
 
+function clearInfoDiv() {
+    while (info.lastChild) {
+        info.removeChild(info.lastChild);
+    }
+}
+
+function addTitle(title) {
+    var h1 = document.createElement('h1');
+    h1.textContent = title;
+    info.appendChild(h1);
+}
+
+function addDefinitions(definitions) {
+    for (let pos in definitions) {
+        var posH2 = document.createElement('h2');
+        posH2.textContent = pos
+        info.appendChild(posH2);
+        var ol = document.createElement('ol');
+        for (let def of definitions[pos]) {
+            var li = document.createElement('li');
+            li.textContent = def;
+            ol.appendChild(li)
+        }
+        info.appendChild(ol);
+    }
+}
+
+var info = document.createElement("div");
+info.id = 'wikt-info';
+document.body.insertBefore(info, document.body.firstChild);
--- background.js
+++ background.js
@@ -20,6 +20,9 @@ var selectionHandler = function (e) {
             }
           }
           alert(infoString);
+          chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+            chrome.tabs.sendMessage(tabs[0].id, {data: info});
+          });
         });
       }
     });
