aa40fb39288faa8c673ae809dbb4b3913030ce81
Tobias Kolditz <tbs.kldtz@gmail.com>
2018-07-26 21:46:02 +0200

    Replace closure with binding
    Instead of getting data into the callback via closures of anonymous
    functions, use a named function and explicitly bind the data to it
    (this).

--- read-russian/js/background.js
+++ read-russian/js/background.js
@@ -1,43 +1,51 @@
 import parseArticle from './wiktParser.js'
 import { findBestResult } from './utils.js'
 
+const EN_WIKT_API = 'https://en.wiktionary.org/w/api.php?';
+const QUERY = 'action=query&format=json&list=search&utf8=1&srwhat=text&srlimit=30&srprop=size&srsearch=';
+
 var selectionHandler = function (e) {
   if (e.selectionText) {
-    var data = { selection: e.selectionText };
-    const url = 'https://en.wiktionary.org/w/api.php?action=query&format=json&list=search&utf8=1&srwhat=text&srlimit=30&srprop=size&srsearch=' + e.selectionText;
-    httpGetAsync(url, function (text) {
-      const json = JSON.parse(text);
-      data.hits = parseInt(json.query.searchinfo.totalhits);
-      if (data.hits === 0) {
-        sendMessage(data);
-        return;
-      }
-      const title = findBestResult(e.selectionText, json.query.search.map(el => el.title));
-      httpGetAsync('https://en.wiktionary.org/wiki/' + title + '?action=raw', function (article) {
-        var info = parseArticle(article, title);
-        info.titles = [title];
-        data.info = info;
-        if (info.inflections) {
-          for (let pos in info.inflections) {
-            let link = info.inflections[pos].normalizedLemma;
-            if (!link) {
-              link = info.inflections[pos].alternative;
-            }
-            if (info.definitions && info.definitions.pos) {
-              continue;
-            }
-            httpGetAsync('https://en.wiktionary.org/wiki/' + link + '?action=raw', function (lemmaArticle) {
-              const lemmaInfo = parseArticle(lemmaArticle, link, new Set([pos]));
-              mergeDefinitions(info, lemmaInfo);
-              sendMessage(data);
-            });
-          }
-        } else {
-          sendMessage(data);
-        }
-      });
-    });
+    const url = EN_WIKT_API + QUERY + e.selectionText;
+    httpGetAsync(url, annotateSelection.bind({ selection: e.selectionText }));
   }
+}
+
+function annotateSelection(text) {
+  const json = JSON.parse(text);
+  this.hits = parseInt(json.query.searchinfo.totalhits);
+  if (this.hits === 0) {
+    sendMessage(this);
+    return;
+  }
+  this.title = findBestResult(this.selection, json.query.search.map(el => el.title));
+  httpGetAsync('https://en.wiktionary.org/wiki/' + this.title + '?action=raw', processBestResult.bind(this));
+}
+
+function processBestResult(article) {
+  var info = parseArticle(article, this.title);
+  info.titles = [this.title];
+  this.info = info;
+  if (info.inflections) {
+    for (let pos in info.inflections) {
+      if (info.definitions && info.definitions.pos) {
+        continue;
+      }
+      let link = info.inflections[pos].normalizedLemma;
+      if (!link) {
+        link = info.inflections[pos].alternative;
+      }
+      httpGetAsync('https://en.wiktionary.org/wiki/' + link + '?action=raw', parseLemmaArticle.bind({data:this, link:link, pos:pos}));
+    }
+  } else {
+    sendMessage(this);
+  }
+}
+
+function parseLemmaArticle(lemmaArticle) {
+  const lemmaInfo = parseArticle(lemmaArticle, this.link, new Set([this.pos]));
+  mergeDefinitions(this.data.info, lemmaInfo);
+  sendMessage(this.data);
 }
 
 function mergeDefinitions(info, newInfo) {
