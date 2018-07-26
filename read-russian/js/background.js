cdd4f0bf3cb020258a2c039af1a23cd5a62f797c
Tobias Kolditz <tbs.kldtz@gmail.com>
2018-07-26 20:18:06 +0200

    Swap page action for browser action

--- read-russian/js/background.js
+++ read-russian/js/background.js
@@ -62,13 +62,6 @@ function sendMessage(data) {
 }
 
 chrome.runtime.onInstalled.addListener(function () {
-  chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
-    chrome.declarativeContent.onPageChanged.addRules([{
-      conditions: [new chrome.declarativeContent.PageStateMatcher({})
-      ],
-      actions: [new chrome.declarativeContent.ShowPageAction()]
-    }]);
-  });
   chrome.contextMenus.create({
     "title": "Get info for '%s'",
     "id": "selectionContextMenu",
